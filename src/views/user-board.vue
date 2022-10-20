0aeb7fde6113b98300433c43e3adb3d6e6b587c9
anna <9408commit@gmail.com>
2022-10-20 12:53:39 +0900

    feat: user-board 기본 틀 완성

--- src/views/user-board.vue
+++ src/views/user-board.vue
@@ -2,17 +2,17 @@
 <div id="user-board">
   <ul class="surveys grid">
     <li
-      v-for="(item, index) in 5" :key="index"
+      v-for="(item, index) in 6" :key="index"
       class="survey-item">
 
       <span class="survey-country grid-only">
-        제목
+        뜨거운 여름밤은 가고 남은건 볼품없지만. 또 다시 찾아오는 누군갈 위해서 남겨두겠소.
       </span>
 
         <div class="pull-right">
           <span class="survey-progress">
             <span class="survey-progress-bg">
-              <span class="survey-progress-fg" style="width: 88%;" />
+              <span class="survey-progress-fg" style="width: 50%;" />
           </span>
 
         <span class="survey-progress-labels">
@@ -23,12 +23,28 @@
           <span class="survey-completes">
             5 / 10
           </span>
+
+<!--          <span class="survey-completes">-->
+<!--            <i class="fa-solid fa-house" /> / <i class="fa-solid fa-house" />-->
+<!--          </span>-->
+
         </span>
       </span>
-
-        <span class="survey-end-date ended">
-          내용
-        </span>
+      </div>
+      <div class="survey-end-date-wrapper">
+      <span class="survey-end-date">
+         잔나비의
+      </span>
+        <br />
+        <span
+          class="survey-end-date">
+        - 뜨거운 여름밤은 가고 남은건 볼품 없지만 -
+      </span>
+        <br />
+        <span
+          class="survey-end-date">
+        중에서
+      </span>
       </div>
     </li>
   </ul>
