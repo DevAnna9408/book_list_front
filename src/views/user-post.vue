6bb776dd43db03301f0fac174301bfde7b35c11d
anna <9408commit@gmail.com>
2022-10-20 13:08:23 +0900

    feat: user-post 기본 틀 완성

--- src/views/user-post.vue
+++ src/views/user-post.vue
@@ -1,6 +1,27 @@
 <template>
 <div id="user-post">
-  글쓰기
+  <ul class="surveys grid">
+    <li class="survey-item">
+      <span class="survey-country grid-only">
+        내용을 입력 해 주세요
+      </span>
+      <div class="survey-end-date-wrapper">
+      <span class="survey-end-date">
+         작가 혹은 가수
+      </span>
+        <br />
+        <span
+          class="survey-end-date">
+        - 제목 -
+      </span>
+      </div>
+      <div
+      style="position: absolute; bottom: 10px; right: 10px;"
+      >
+      버튼
+      </div>
+    </li>
+  </ul>
 </div>
 </template>
 
