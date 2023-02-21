0aa311bd2ad22ea4848310627473b87c7b6af749
anna <9408commit@gmail.com>
2023-02-21 20:16:16 +0900

    refactor: 불필요한 스타일 및 클래스 정리

--- src/App.vue
+++ src/App.vue
@@ -12,20 +12,17 @@
 </template>
 <script>
 import { mapGetters } from 'vuex'
-// import { isChrome } from '@/utils/browserCheck'
 export default {
   name: 'App',
   computed: {
     ...mapGetters({ loading: 'loading' })
   },
   created () {
-    // backspace input, textarea 아니면 적용안됨
     window.addEventListener('keydown', e => {
       if (e.key === 'Backspace' && e.target.nodeName !== 'INPUT' && e.target.nodeName !== 'TEXTAREA') {
         e.preventDefault()
       }
     })
-    // if (!isChrome()) { alert('지원하지 않는 브라우저 입니다. 크롬 브라우저를 사용해주세요.\nThis browser is not supported.Please use a chrome browser') }
   }
 }
 </script>
